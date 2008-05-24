1c1e58a58e14163d0c7c49e1d0ccccbf09b3906a
cho45 <cho45@lowreal.net>
2008-05-24 12:59:08 +0900

    Show unpushed remotes.

--- gitalone.rb
+++ gitalone.rb
@@ -8,6 +8,43 @@ require "mojombo-grit"
 require "pathname"
 require "ostruct"
 
+### --start
+module Grit
+	class Remote
+		attr_reader :name, :repo
+
+		def self.find_all(repo)
+			Pathname.glob("#{repo.path}/refs/remotes/*").map { |path|
+				Remote.new(repo, path.basename.to_s)
+			}
+		end
+
+		def initialize(repo, name)
+			@repo, @name = repo, name
+		end
+
+		def heads
+			Pathname("#{@repo.path}/refs/remotes/#{@name}").children.map {|c|
+				Grit::Head.new(c.basename.to_s, Grit::Commit.create(@repo, :id => c.read.strip))
+			}
+		end
+
+		def head(name="master")
+			c = Pathname.new("#{@repo.path}/refs/remotes/#{@name}/#{name}")
+			Grit::Head.new(c.basename.to_s, Grit::Commit.create(@repo, :id => c.read.strip))
+		rescue Errno::ENOENT, Errno::ENOTDIR
+			nil
+		end
+	end
+
+	class Repo
+		def remotes
+			Remote.find_all(self)
+		end
+	end
+end
+### --end
+
 class GitAlone < Controller
 	def self.call(env)
 		new(:dir => "~/project/").result(env)
@@ -36,8 +73,8 @@ class GitAlone < Controller
 			repo = Grit::Repo.new(g)
 			next unless repo.heads.first
 			stash[:repos] << {
-				:path => g.parent,
-				:repo => repo
+				:path    => g.parent,
+				:repo    => repo,
 			}
 		end
 		GC.start
